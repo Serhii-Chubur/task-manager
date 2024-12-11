6e51f30c3343506aa02d9d76e13f3b764639e284
Serhii-Chubur <serhichubur15@gmail.com>
2024-12-11 16:08:23 +0200

    add models

--- task_tracker/models.py
+++ task_tracker/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class Position(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('Urgent', 'Urgent'),
+        ('High', 'High'),
+        ('Medium', 'Medium'),
+        ('Low', 'Low'),
+    ]
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='Medium')
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker)
+
